32413d8052e1b4c6157d8286934a6f9a3bed54e7
Sebastián <nparodi@gmail.com>
2020-09-05 22:14:24 +0200

    Better documented version

--- automove.py
+++ automove.py
@@ -1,12 +1,17 @@
 #¡ /usr/bin/python3.7
 
+###########################################################
+#  Automove (and sort) files after being dropped in a folder
+#  Developed by Sebastián Nicolás Parodi
+#  Email : nparodi@gmail.com
+#  Started on Sept 2020
+###########################################################
+
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from datetime import datetime
 
 import os
-
-import json
 import time
 
 class MyHandler(FileSystemEventHandler):
@@ -14,41 +19,38 @@ class MyHandler(FileSystemEventHandler):
         for filename in os.listdir(folder_to_track):
 
             new_destination = None
-
             src = folder_to_track + "/" + filename
-
-           # print
-            fullfile = folder_to_track + filename
-            date_creation = datetime.strptime(time.ctime(os.path.getctime(fullfile)),"%a %b %d %H:%M:%S %Y")
-
             extension = os.path.splitext(filename)[1]
 
-            print(filename)
-            print(extension.lower())
-            print(extension)
-
             if extension.lower().endswith(('.png','.jpg','.jpeg')):
-                folder_destination = folder_images + "/" + \
-                                     str(date_creation.year) + "_" \
-                                     + str(date_creation.month) + "/"
+                # In case of images i will put them in a sub folder depending on file modification date
+                last_modification = datetime.strptime(time.ctime(os.path.getmtime(src)), "%a %b %d %H:%M:%S %Y")
 
+                folder_destination = folder_images  \
+                                     str(last_modification.year) + "_" \
+                                     + str(last_modification.month) + "/"
+
+                #Checks if folder allready exists
                 if not os.path.exists(folder_destination):
                     os.makedirs(folder_destination)
+
                 new_destination = folder_destination + filename
 
 
             elif extension.lower().endswith(('.pdf')):
-                new_destination = folder_pdf + "/" + filename
+                # In case of PDF =
+                new_destination = folder_pdf  + filename
 
 
             if new_destination:
+                # Moves file to new destination
                 os.rename(src, new_destination)
 
 
-
-folder_to_track = "/home/bonzo/Documentos/"
-folder_images = "/home/bonzo/Documentos/Imagenes"
-folder_pdf = "/home/bonzo/Documentos/PDF"
+# Folders to track and destiny folders
+folder_to_track = os.path.expanduser('~') + "/Documentos/"
+folder_images = folder_to_track + "Imagenes/"
+folder_pdf = folder_to_track + "PDF/"
 
 event_handler = MyHandler()
 observer = Observer()
